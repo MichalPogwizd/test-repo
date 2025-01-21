a040c866909ff6724e71b138304b17c99230f2c7
MichalPogwizd <pogwizd.michal1@gmail.com>
2025-01-21 19:01:32 +0100

    added abs_value method and corresponding test

--- examples/maths.py
+++ examples/maths.py
@@ -2,3 +2,7 @@ class Calc():
     @staticmethod
     def square(n):
         return n*n
+    
+    @staticmethod
+    def abs_value(n):
+        return abs(n)
--- tests/test_maths.py
+++ tests/test_maths.py
@@ -5,3 +5,6 @@ from examples.maths import Calc
 
 def test_square():
     assert Calc.square(9) == 81
+
+def test_abs():
+    assert Calc.abs_value(-9) == 9
